02fe56207d3e3136b210ac09491d8804c356226f
rohitagr1 <126511766+rohitagr1@users.noreply.github.com>
2025-11-02 00:39:30 +0530

    Add .gitignore and remove unnecessary tracked files (#2)
    * Add .gitignore and remove unnecessary tracked files
    
    * Update .gitignore and remove ignored files

---
+++ sql/createTable3.js
@@ -0,0 +1,26 @@
+import sqlite3  from "sqlite3";
+import { open } from "sqlite";
+import path from 'node:path';
+
+async function createTable3(){
+    const db = await open({
+        filename : path.join('database.db'),
+        driver : sqlite3.Database
+    })
+
+    await db.exec(`
+        CREATE TABLE cart_items(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_id INTEGER NOT NULL,
+                product_id INTEGER NOT NULL,
+                quantity INTEGER NOT NULL DEFAULT 1,
+                FOREIGN KEY (user_id) REFERENCES users(id),
+                FOREIGN KEY (product_id) REFERENCES products(id)
+            );
+        `)
+
+    await db.close()
+    console.log("table created")
+}
+
+createTable3()
